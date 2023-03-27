2d37ace9ff6da68590237c0fab435e391aeb6298
Jorge Roa <64996320+jurjoroa@users.noreply.github.com>
2023-03-27 23:27:41 +0200

    Final update

--- cq.py
+++ cq.py
@@ -1,43 +1,105 @@
-# -*- coding: utf-8 -*-
+# -- coding: utf-8 --
 """
 Created on Tue Mar 14 15:42:53 2023
 
-
-@author: Hannah
+@author: Jorge Roa & Carmen Garro
 """
 
 class Empty(Exception):
-  pass
-
+    pass
 
 class CircularQueue:
-    """Queue implementation using circularly linked list for storage"""""
-    
-    class _Node:
-        __slots__ = '_element', '_next'
-        
-        def __init__(self, element, nxt):
-            self._element = element
-            self._next = nxt
+    """Queue implementation"""
 
+    # Define the Node class for the circularly linked list
+    class _Node:
+        __slots__ = 'element', 'next'
+
+        def __init__(self, element, nxt):
+            self.element = element
+            self.next = nxt
+
+    # Initialize the CircularQueue object
     def __init__(self):
         self._tail = None
         self._size = 0
 
+    # Return the length (size) of the queue
     def __len__(self):
         return self._size
 
+    # Check if the queue is empty
+    @property
     def is_empty(self):
         return self._size == 0
 
+    # Get the first element in the queue
+    @property
     def first(self):
-       pass
+       if self.is_empty:
+           raise Empty("The queue is empty")
+       head = self._tail.next
+       return head.element
 
+    # Remove and return the first element from the queue
     def dequeue(self):
-        pass
-    
+        if self.is_empty:
+            raise Empty("The queue is empty")
+        former_head = self._tail.next
+        dequeued_element = former_head.element
+        if self._size == 1:
+            self._tail = None
+        else:
+            self._tail.next = former_head.next
+        self._size -= 1
+        return dequeued_element
+
+    # Add a new element to the end of the queue
     def enqueue(self, e):
-        pass
-    
+        newest = self._Node(e, None)
+        if self.is_empty:
+            newest.next = newest
+        else:
+            newest.next = self._tail.next
+            self._tail.next = newest
+        self._tail = newest
+        self._size += 1
+
+    # Move the first element to the end of the queue
     def rotate(self):
-       pass
+        if self._size > 0:
+            self._tail = self._tail.next
+
+    # Provide a formal string representation of the CircularQueue object
+    def __repr__(self):
+        return f"CircularQueue(size={self._size})"
+
+    # Provide a readable string representation of the CircularQueue object
+    def __str__(self):
+        if self.is_empty:
+            return "Empty Queue"
+        result = []
+        current = self._tail.next
+        for _ in range(self._size):
+            result.append(str(current.element))
+            current = current.next
+        return " -> ".join(result)
+
+    # Check if a value is present in the queue using the 'in' keyword
+    def __contains__(self, value):
+        if self.is_empty:
+            return False
+        current = self._tail.next
+        for _ in range(self._size):
+            if current.element == value:
+                return True
+            current = current.next
+        return False
+
+    # Look at the next element to be dequeued without removing it
+    def peek(self):
+        if self.is_empty:
+            raise Empty("The queue is empty")
+        return self._tail.next.element
+
+
--- test.py
+++ test.py
@@ -1,18 +1,54 @@
-# -*- coding: utf-8 -*-
+# -- coding: utf-8 --
 """
-Created on Tue Mar 14 15:48:01 2023
+Created on Tue Mar 14 15:42:53 2023
 
-@author: Hannah
+@author: Jorge Roa & Carmen Garro
 """
 
 from cq import CircularQueue
 
-cq = CircularQueue()
-print("Starting Size", len(cq) )
-cq.enqueue("a")
-cq.enqueue("b")
-cq.enqueue("c")
-print("Current Size", len(cq) )
-print("Current Head", cq.first())
-cq.rotate()
-print("Current Head after rotating", cq.first())
+#Example 1
+cirqe = CircularQueue()
+cirqe.enqueue(1)
+cirqe.enqueue(2)
+cirqe.enqueue(3)
+print(cirqe)
+
+
+#Example 2
+
+# Use the rotate method to move the first element to the end of the queue
+cirqe.rotate()
+print("Queue after rotate:", cirqe)
+
+# Dequeue the element and display the updated queue
+dequeued_element = cirqe.dequeue()
+print("Dequeued element:", dequeued_element)
+print("Queue after dequeue:", cirqe)
+
+
+#Example 3: Look at the next element to be dequeued without removing it
+
+# Create a new CircularQueue instance
+cirqe = CircularQueue()
+
+# Enqueue some elements
+cirqe.enqueue(1)
+cirqe.enqueue(2)
+cirqe.enqueue(3)
+
+# Display the current state of the queue
+print("Current Queue:", cirqe)
+
+# Use the peek method to look at the next element without dequeuing it
+next_element = cirqe.peek()
+print("Next element to be dequeued:", next_element)
+
+# Display the queue again to show that the element has not been dequeued
+print("Queue after peek:", cirqe)
+
+# Dequeue the element and display the updated queue
+dequeued_element = cirqe.dequeue()
+print("Dequeued element:", dequeued_element)
+print("Queue after dequeue:", cirqe)
+
